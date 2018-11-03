91a214915c9cf05cf3a6a09ca34f9ad52a30e20a
shimizu <s-shimizu@cs-fact.com>
2018-11-03 17:17:39 +0900

    こみっと

--- calculator/src/main.js
+++ calculator/src/main.js
@@ -1,8 +1,12 @@
 var clickButtton = function (type) {
+    const ekisyou1 = document.getElementById("ekisyou1");
     const ekisyou2 = document.getElementById("ekisyou2");
-    // switch (type) {
-    //     case :
-    //     break;
-    // }
-    ekisyou2.innerHTML = type;
+    switch (type) {
+        case 'CE':
+        case 'C':
+        ekisyou1.innerHTML = ''
+        ekisyou2.innerHTML = ''
+        break;
+    }
+    ekisyou2.innerHTML = ekisyou2.innerHTML + type;
 }
